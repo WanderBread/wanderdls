692e2fe2f8fd186721cbc85790b3a51a1e3a8e77
Wander <delossantos.wander@gmail.com>
2017-06-30 02:51:22 -0400

    last commit on June 29th 2017

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  selector: 'app',
+  template:`
+  <home></home>
+  <about></about>
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { name = 'Wander'; }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { HomeComponent } from './components/home.component';
+import { AboutComponent } from './components/about.component';
+
+
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, HomeComponent, AboutComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
